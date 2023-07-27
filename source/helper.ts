6990af5fddc5de305aca2f1322a17cc451037706
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-07-28 01:17:37 +1000

    +Better CSS typing

--- source/helper.ts
+++ source/helper.ts
@@ -1,9 +1,14 @@
-export function StyleCSS(props: { [key: string]: string | number }) {
+import type * as CSS from 'csstype';
+
+export function StyleCSS(props: CSS.Properties<string | number>) {
 	let out = "";
 
 	for (const key in props) {
+		const value = props[key as keyof CSS.Properties<string | number>];
+		if (typeof(value) !== "string" && typeof(value) !== "number" ) continue;
+
 		const safeKey = key.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
-		const safeVal = props[key].toString().replace(/"/g, "\\\"");
+		const safeVal = value.toString().replace(/"/g, "\\\"");
 		out += `${safeKey}: ${safeVal};`
 	}
 

61b41fee6dadaab6307a495c5254901a2d3a1ee9
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-08-12 11:34:35 +1000

    +allow class name

--- source/components.tsx
+++ source/components.tsx
@@ -2,11 +2,13 @@ import * as elements from 'typed-html';
 
 export function Link(props: {
 	to: string,
+	class?: string,
 	target?: string,
 	style?: string
 }, contents: string[]) {
 	return <a
 		target={props.target || ""}
+		class={props.class || ""}
 		style={props.style || ""}
 		href={props.to}
 		hx-get={props.to}

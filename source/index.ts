b71abfb263ac0b59ca990643df165def55e1fb83
Ajani Bilby <me@ajanibilby.com>
2024-12-21 17:02:54 +1100

    +meta rendering

--- source/index.ts
+++ source/index.ts
@@ -2,26 +2,39 @@ import { RouteModule, CatchFunction, RenderFunction } from '~/types.js';
 import { RouteContext, GenericContext } from "~/router.js";
 import { createRequestHandler } from '~/request/index.js';
 
+import {
+	MetaDescriptor, RenderMetaDescriptor, ShellOptions, MergeShellOptions,
+	LdJsonObject, OpenGraph, OpenGraphImage, OpenGraphVideo, OpenGraphAudio
+} from '~/util/shell.js';
+import { redirect, refresh, revalidate, text, json } from '~/util/response.js';
 import { Cookies, CookieOptions } from "~/util/cookies.js";
 import { EventSourceConnection } from "~/util/event-source.js";
 import { DynamicReference } from '~/util/dynamic.js';
 import { StyleClass } from '~/util/css.js';
 import { Endpoint } from '~/util/endpoint.js';
 
-import { redirect, refresh, revalidate, text, json } from '~/util/response.js';
 
 export {
-	CatchFunction,
-	CookieOptions,
-	Cookies,
 	createRequestHandler,
-	DynamicReference,
-	Endpoint,
-	EventSourceConnection,
-	GenericContext,
-	RenderFunction,
-	RouteContext,
-	RouteModule,
+
+	// Router types
+	CatchFunction, RenderFunction,
+	RouteContext, RouteModule, GenericContext,
+
+	// Request helpers
+	Cookies, CookieOptions,
+	Endpoint, DynamicReference,
+
+	// CSS Helper
 	StyleClass,
+
+	// SSE helper
+	EventSourceConnection,
+
+	// Response helpers
 	redirect, refresh, revalidate, text, json,
+
+	// Meta + shell helpers
+	MetaDescriptor, RenderMetaDescriptor, ShellOptions, MergeShellOptions,
+	LdJsonObject, OpenGraph, OpenGraphImage, OpenGraphVideo, OpenGraphAudio
 };

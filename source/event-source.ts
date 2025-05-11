22c4359d1f0e6008005ff616b8fa03ba93e29e59
Ajani Bilby <me@ajanibilby.com>
2025-05-12 09:51:36 +1000

    +increased compliance

--- source/event-source.ts
+++ source/event-source.ts
@@ -1,8 +1,6 @@
 import { ServerOnlyWarning } from "./internal/util.js";
 ServerOnlyWarning("event-source");
 
-type Controller = ReadableStreamDefaultController;
-
 
 // global for easy reuse
 const encoder = new TextEncoder();
@@ -12,7 +10,7 @@ headers.set("X-Accel-Buffering", "no");
 headers.set("Transfer-Encoding", "chunked");
 headers.set("Content-Type", "text/event-stream");
 // the maximum keep alive chrome shouldn't ignore
-headers.set("Keep-Alive", "timeout=120");
+headers.set("Keep-Alive", "timeout=60");
 headers.set("Connection", "keep-alive");
 
 
@@ -21,39 +19,53 @@ headers.set("Connection", "keep-alive");
  * Includes a keep alive empty packet sent every 30sec (because Chrome implodes at 120sec, and can be unreliable at 60sec)
  */
 export class EventSource {
-	private controller: Controller | null;
-	private timer: NodeJS.Timeout | null;
-	private state: number;
+	#controller: ReadableStreamDefaultController | null;
+	#timer: NodeJS.Timeout | null;
+	#state: number;
 
 	readonly response: Response;
-	readonly url: string; // just to make it polyfill
+
+	readonly createdAt: number; // when was this source created
+	#updatedAt: number;
+
+	// just to make it polyfill
+	readonly withCredentials: boolean;
+	readonly url: string;
 
 	constructor(request: Request, keepAlive = 30_000) {
-		this.controller = null;
-		this.state = 0;
+		this.#controller = null;
+		this.#state = EventSource.CONNECTING;
 		this.url = request.url;
+		this.withCredentials = request.mode === "cors";
+
+		this.createdAt = Date.now();
+		this.#updatedAt = 0;
 
 		const stream = new ReadableStream({
-			start: (c) => { this.controller = c; this.state = 1; },
+			start: (c) => { this.#controller = c; this.#state = 1; },
 			cancel: () => { this.close(); }
 		});
-		request.signal.addEventListener('abort', () => this.close());
+		request.signal.addEventListener('abort', () => { this.close() });
 
 		this.response = new Response(stream, { headers });
 
-		this.timer = setInterval(() => this.keepAlive(), keepAlive);
+		this.#timer = setInterval(() => this.keepAlive(), keepAlive);
 		register.add(this);
 	}
 
 	get readyState() {
-		return this.state;
+		return this.#state;
+	}
+	get updatedAt () {
+		return this.#updatedAt;
 	}
 
-	private sendBytes(chunk: Uint8Array) {
-		if (!this.controller) return false;
+	private sendBytes(chunk: Uint8Array, active: boolean) {
+		if (!this.#controller) return false;
 
 		try {
-			this.controller.enqueue(chunk);
+			this.#controller.enqueue(chunk);
+			if (active) this.#updatedAt = Date.now();
 			return true;
 		} catch (e) {
 			console.error(e);
@@ -62,38 +74,46 @@ export class EventSource {
 		}
 	}
 
-	private sendText(chunk: string) {
-		return this.sendBytes(encoder.encode(chunk));
+	private sendText(chunk: string, simulated: boolean) {
+		return this.sendBytes(encoder.encode(chunk), simulated);
 	}
 
 	private keepAlive() {
-		return this.sendText("\n\n");
+		return this.sendText("\n\n", false);
 	}
 
 	dispatch(type: string, data: string) {
-		return this.sendText(`event: ${type}\ndata: ${data}\n\n`);
+		return this.sendText(`event: ${type}\ndata: ${data}\n\n`, true);
 	}
 
 	close (unlink = true) {
-		if (this.state === 2) return false;
+		if (this.#state === 2) {
+			this.#controller = null;
+			return false;
+		}
 
 		if (unlink) register.delete(this);
 
 		try {
-			this.controller?.close();
+			this.#controller?.close();
 		} catch (e) {
 			console.error(e);
-			this.controller = null;
+			this.#controller = null;
+			this.#state = 2;
 			return false;
 		}
 
 		// Cleanup
-		if (this.timer) clearInterval(this.timer);
-		this.controller = null;
-		this.state = 2;
+		if (this.#timer) clearInterval(this.#timer);
+		this.#controller = null;
+		this.#state = 2;
 
 		return true;
 	}
+
+	static CONNECTING = 0;
+	static OPEN       = 1;
+	static CLOSED     = 2;
 }
 
 export class EventSourceSet extends Set<EventSource> {

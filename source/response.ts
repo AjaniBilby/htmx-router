f8598f36c91d7fe36eb2463ceea24543ef535c9b
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2025-08-02 11:36:00 +1000

    ~fixed deno response bug

--- source/response.ts
+++ source/response.ts
@@ -1,7 +1,5 @@
 export function text(text: BodyInit, init?: ResponseInit) {
-	init ||= {};
-	init.statusText ||= "ok";
-	init.status ||= 200;
+	init = FillResponseInit(200, "Ok", init);
 
 	const res = new Response(text, init);
 	res.headers.set("Content-Type", "text/plain");
@@ -11,9 +9,7 @@ export function text(text: BodyInit, init?: ResponseInit) {
 }
 
 export function html(text: BodyInit, init?: ResponseInit) {
-	init ||= {};
-	init.statusText ||= "ok";
-	init.status ||= 200;
+	init = FillResponseInit(200, "Ok", init);
 
 	const res = new Response(text, init);
 	res.headers.set("Content-Type", "text/html; charset=UTF-8");
@@ -25,9 +21,7 @@ export function html(text: BodyInit, init?: ResponseInit) {
 export type TypedResponse<T> = Omit<Response, "json"> & { json(): Promise<T> };
 export type TypedJson<U extends TypedResponse<any>> = U extends TypedResponse<infer T> ? T : never;
 export function json<T>(data: T, init?: ResponseInit): TypedResponse<T> {
-	init ||= {};
-	init.statusText ||= "ok";
-	init.status ||= 200;
+	init = FillResponseInit(200, "Ok", init);
 
 	const res = new Response(JSON.stringify(data), init);
 	res.headers.set("Content-Type", "application/json");
@@ -37,36 +31,47 @@ export function json<T>(data: T, init?: ResponseInit): TypedResponse<T> {
 }
 
 export function redirect(url: string, init?: ResponseInit & { clientOnly?: boolean }) {
-	init ||= {};
-	init.statusText ||= "Temporary Redirect";
-	init.status ||= 307;
+	init = FillResponseInit(307, "Temporary Redirect", init);
 
 	const res = new Response("", init);
 	if (!init?.clientOnly) res.headers.set("Location", url);
 	res.headers.set("HX-Location", url); // use hx-boost if applicable
+	res.headers.set("X-Caught", "true");
 
 	return res;
 }
 
 export function revalidate(init?: ResponseInit) {
-	init ||= {};
-	init.statusText ||= "ok";
-	init.status ||= 200;
+	init = FillResponseInit(200, "Ok", init);
 
 	const res = new Response("", init);
 	res.headers.set("HX-Location", "");
+	res.headers.set("HX-Replace-Url", "");
+	res.headers.set("X-Caught", "true");
 
 	return res;
 }
 
 export function refresh(init?: ResponseInit & { clientOnly?: boolean }) {
-	init ||= {};
-	init.statusText ||= "ok";
-	init.status ||= 200;
+	init = FillResponseInit(200, "Ok", init);
 
 	const res = new Response("", init);
 	if (!init?.clientOnly) res.headers.set("Refresh", "0"); // fallback
 	res.headers.set("HX-Refresh", "true");
+	res.headers.set("X-Caught", "true");
 
 	return res;
+}
+
+
+/**
+ * This is to fix issues with deno
+ * When you try and change the statusText on a Response object
+ */
+function FillResponseInit(status: number, statusText: string, init: ResponseInit | undefined) {
+	if (init === undefined) init = {};
+	if (init.statusText === undefined) init.statusText = statusText;
+	if (init.status === undefined)     init.status = status;
+
+	return init;
 }

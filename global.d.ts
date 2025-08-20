af2c3ca957658e6e2b4bea631b79d3e0afafa97e
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2025-08-20 14:13:37 +1000

    ~use vite envs

--- global.d.ts
+++ global.d.ts
@@ -5,3 +5,22 @@ declare namespace JSX {
 	}
 	interface Fragment {}
 }
+
+interface ImportMeta {
+	glob: (
+		pattern: string,
+		options?: {
+			eager?: boolean;
+			import?: string;
+			as?: string;
+		}
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	) => Record<string, any>;
+	env: {
+		BASE_URL: string,
+		DEV: boolean,
+		MODE: string,
+		PROD: boolean,
+		SSR: true,
+	} & Record<`VITE_${string}`, string>
+}
--- source/internal/client.ts
+++ source/internal/client.ts
@@ -4,7 +4,7 @@ ServerOnlyWarning("client-url");
 import { readFile } from "fs/promises";
 
 export async function GetClientEntryURL() {
-	if (process.env.NODE_ENV !== "production") return "/app/entry.client.ts";
+	if (import.meta.env.DEV) return "/app/entry.client.ts";
 
 	const config = JSON.parse(await readFile("./dist/client/.vite/manifest.json", "utf8"));
 	for (const key in config) {
